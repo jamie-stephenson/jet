6e6b085f1b894464d69cda0b00a6c52cb819f20a
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-15 16:07:59 +0100

    Fix: typo

--- scripts/build_cluster.sh
+++ scripts/build_cluster.sh
@@ -97,7 +97,7 @@ run_on_node() {
         if ! ssh-keygen -F $node; then
             ssh-keyscan -t ed25519 -H $node >> ~/.ssh/known_hosts
         fi
-        ssh -i ~/.ssh/id_ed25519 $USER@$node "bash -s -- ${arr[@]}" < $script > $output_file 2>&1
+        ssh -i ~/.ssh/id_ed25519 $USER@$node "bash -s -- ${args[@]}" < $script > $output_file 2>&1
     fi
 }
 
