0ab5f25e8223d8205b793832895311b2af32b3dc
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-18 12:03:54 +0100

    Added option to download fineweb separately.
    Instead of downloading it the first time you run
    encode.py, you now have the option to download
    fineweb explicitly by running fineweb-edu.py as main

--- project_datasets/fineweb-edu.py
+++ project_datasets/fineweb-edu.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from src.dist_utils import find_opus_indices
 import torch.distributed as dist
+import argparse
 
 def get_dataset(path,rank,world_size):
     start, end = find_opus_indices(100000,rank,world_size) # We train on 100,000 documents TODO: make this an arg
@@ -20,3 +21,29 @@ def get_dataset(path,rank,world_size):
             cache_dir=path
         )
     return '\n'.join(dataset['text'])
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--path",
+        type=str,
+        help="Path to dir to download data to."
+    )
+
+    parser.add_argument(
+        "--num_proc",
+        type=str,
+        help="Number of processes to use for downloading."
+    )
+
+    args = parser.parse_args()
+
+    load_dataset(
+            path='HuggingFaceFW/fineweb-edu',
+            name='sample-10BT',
+            split='train',
+            cache_dir=args.path,
+            num_proc=args.num_proc
+    )
