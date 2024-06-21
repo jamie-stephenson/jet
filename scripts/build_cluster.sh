53e3a441208ac508eea5de759b66966d89d9ebc1
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-21 15:47:13 +0100

    Fix: Could not get lock (unattended upgrade)
    This was fixed in `build_worker.sh` but also occurs sometimes during execution of
    `build_cluster.sh` the wait function is added here too.

--- scripts/build_cluster.sh
+++ scripts/build_cluster.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+wait_for_pkg_mngmt() {
+    echo "Checking for running package management processes..."
+    while sudo lsof /var/lib/dpkg/lock-frontend /var/lib/apt/lists/lock /var/cache/apt/archives/lock >/dev/null 2>&1; do
+        echo "Package management processes are running. Waiting for them to finish..."
+        sleep 5
+    done
+    echo "No package management processes running. Continuing with the script..."
+}
+
 sudo apt-get update
+wait_for_pkg_mngmt
 
 #--RUN CONFIG SCRIPT--
 config_file="cluster_config.sh"
