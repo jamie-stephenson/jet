e155f8554f74e7ca0c4d38cd519b1726e1077e79
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-28 00:44:35 +0100

    Added val split to fineweb_edu

--- project_datasets/fineweb_edu.py
+++ project_datasets/fineweb_edu.py
@@ -2,18 +2,28 @@ from datasets import load_dataset
 from src.dist_utils import find_opus_indices
 import os
 
-def get_dataset(path,rank,world_size):
+#-----CONFIG------
+val_size = 1e4   # number of documents in the val split
+train_size = 1e5 # We train on 100,000 documents 
+#-----------------
+
+def get_dataset(path,split,rank,world_size):
     assert os.path.exists(path), (
         f"Dataset fineweb-edu does not exist at {path}.\n"
         f"Please download by running `python download.py --corpus fineweb_edu`"
     ) #TODO stop this from duplicating across processes while still ending process group gracefully
 
-    start, end = find_opus_indices(100000,rank,world_size) # We train on 100,000 documents TODO: make this an arg
+    if split == 'train':
+        start, end = find_opus_indices(100000,rank,world_size) 
+        start += val_size
+        end += val_size
+    else:
+        start, end = find_opus_indices(val_size,rank,world_size)
 
     dataset = load_dataset(
         path='HuggingFaceFW/fineweb-edu',
         name='sample-10BT',
-        split=f'train[{start}:{end}]',
+        split=f'train[{start}:{end}]', # I think this dataset only has a train split so we make our own val split
         cache_dir=path
     )
 
