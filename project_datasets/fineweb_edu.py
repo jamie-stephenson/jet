9f7081d4cef85014cc59c700b24480d0a68f162e
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-28 17:18:48 +0100

    Removing support for different splits from fineweb
    This will be handled by data_utils.py instead

--- project_datasets/fineweb_edu.py
+++ project_datasets/fineweb_edu.py
@@ -3,22 +3,16 @@ from src.dist_utils import find_opus_indices
 import os
 
 #-----CONFIG------
-val_size = 1e4   # number of documents in the val split
 train_size = 1e5 # We train on 100,000 documents 
 #-----------------
 
-def get_dataset(path,split,rank,world_size):
+def get_dataset(path,rank,world_size):
     assert os.path.exists(path), (
         f"Dataset fineweb-edu does not exist at {path}.\n"
         f"Please download by running `python download.py --corpus fineweb_edu`"
     ) #TODO stop this from duplicating across processes while still ending process group gracefully
 
-    if split == 'train':
-        start, end = find_opus_indices(100000,rank,world_size) 
-        start += val_size
-        end += val_size
-    else:
-        start, end = find_opus_indices(val_size,rank,world_size)
+    start, end = find_opus_indices(train_size,rank,world_size)
 
     dataset = load_dataset(
         path='HuggingFaceFW/fineweb-edu',
