58def5a5f1b8ff09d8ec033adf805b86b022dc61
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-06-29 01:22:25 +0100

    Typo

--- infra/scripts/build_worker.sh
+++ infra/scripts/build_worker.sh
@@ -31,7 +31,7 @@ sudo systemctl start slurmd
 #-PYTHON ENVIRONMENT--
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -o DPkg::Lock::Timeout=60 -y install python3.11
-sudo apt-get -o DPkg::Lock::Timeout=60 -y python3.11-venv
+sudo apt-get -o DPkg::Lock::Timeout=60 -y install python3.11-venv
 mkdir envs
 python3.11 -m venv ~/envs/jet
 source ~/envs/jet/bin/activate
