1bc7d1e952eed73f7c55aa6cbc5db4c423122a05
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-07-17 20:07:28 +0100

    Improved ShardedDataset
    Better shuffling using two different sources of rng (global and rankwise)
    Will hopefully handle different loader lengths on different ranks by using
    the `model.join()` context manager

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,7 +5,7 @@ import os
 
 class ArrayDataset(Dataset):
     """
-    Dataset to sample blocks of tokens of size `seq_len` from an array of tokens.
+    Dataset to sample sequences of tokens of size `seq_len` from an array of tokens.
     "Array" can be anything indexed using slice notation ('arr[start:end]') and that 
     `torch.tensor` accepts as valid 'data', e.g. list, np.array, np.memmap.
     """
@@ -48,18 +48,32 @@ class CustomBatchSampler(Sampler):
     
 class ShardedDataset(IterableDataset):
     """
-    Dataset to sample blocks of tokens of size `seq_len` from a directory containing numpy shards.
-    Each epoch all shards will be shuffled and shared across all available ranks.
+    Dataset to sample sequences of tokens of size `seq_len` from a directory containing numpy shards.
+    Each epoch all* shards will be shuffled and shared across all available ranks.
     Each rank will then shuffle and then iterate over each of its shards.
+
+    * (not quite all: see `nshards_per_epoch` below)
     """
     def __init__(self, shard_paths, seq_len, overlap, rank, world_size):
         self.seq_len = seq_len
         self.idx_step = seq_len - overlap
+
         self.rank = rank
         self.world_size = world_size 
+
         self.shard_paths = shard_paths
-        self.global_seed = torch.initial_seed() 
-        self.worker_seed = torch.initial_seed()
+        self.nshards = len(shard_paths)
+
+        # To minimise differnce in dataloader length on each rank, each
+        # epoch all ranks will form dataset from same number of shards.
+        # This means the total number of shards we consider each epoch
+        # needs to be a multiple of world_size: 
+        self.nshards_per_epoch = world_size * (self.nshards // world_size) 
+
+        # The following attributes will be set by the worker_init_fn (iff num_workers > 0):
+        self.worker_id = 0
+        self.num_workers = 1
+        self.rank_rng = None
         
     def _load_shard(self, shard_path):
         """Generator to yield sequences of tokens from a given shard"""
@@ -67,27 +81,39 @@ class ShardedDataset(IterableDataset):
         torch_data = torch.from_numpy(data)
         max_idx = len(torch_data) - self.seq_len - 1
         ids = torch.arange(0,max_idx,self.idx_step)
-        print(torch.randperm(10))
-        shuffled_ids = ids[torch.randperm(len(ids))] # This shuffle is different on all ranks 
+        # This shuffle is different on all ranks but the same for all workers on a given rank:
+        shuffled_ids = ids[torch.randperm(len(ids),generator=self.rank_rng)[self.worker_id::self.num_workers]] 
         for idx in shuffled_ids:
             sample = torch_data[idx:idx+self.seq_len]
             targets = torch_data[idx+1:idx+self.seq_len+1]
             yield sample, targets
 
     def __iter__(self):
+        # This shuffle is the same on all ranks:
+        shuffled_path_ids = torch.randperm(self.nshards)[:self.nshards_per_epoch] 
+        shard_paths = [self.shard_paths[i] for i in shuffled_path_ids]  
 
-        torch.manual_seed(self.global_seed)
-        self.shard_paths = [self.shard_paths[i] for i in torch.randperm(len(self.shard_paths))]  # This shuffle is the same on all ranks (as desired)
-        torch.manual_seed(self.worker_seed)
-
-        shard_paths = self.shard_paths[self.rank::self.world_size]
+        shard_paths = shard_paths[self.rank::self.world_size]
         for shard_path in shard_paths:
+            print(self.worker_id,shard_path)
             yield from self._load_shard(shard_path)
 
-def sharded_init_fn(worker_id): 
-    dataset = torch.utils.data.get_worker_info().dataset
-    dataset.worker_seed += worker_id
-    pass
+def seed_worker(worker_id):
+    """
+    This is a worker_init_fn that establishes two sources of rng for use in dataloader workers:
+    1) The global torch source of rng. This means that all workers on all ranks can perform the same shuffles when needed.
+    2) A local source of rng that is the same for all workers on a given rank but different for different ranks.
+    """
+    worker_info = torch.utils.data.get_worker_info()
+    dataset =  worker_info.dataset
+    dataset.worker_id = worker_id
+    dataset.num_workers = worker_info.num_workers
+
+    global_seed = torch.initial_seed()-worker_id # Undo torch's automatic seeding
+    torch.manual_seed(global_seed)
+
+    dataset.rank_rng = torch.Generator() # Create second source of rng that is rankwise unique
+    dataset.rank_rng.manual_seed(global_seed+dataset.rank)
 
 def get_dataloader(path,split,args):     
     if args.encoded_format == 'mmap':
@@ -100,7 +126,12 @@ def get_dataloader(path,split,args):
     elif args.encoded_format == 'shards':
         paths = [os.path.join(path,shard) for shard in sorted(os.listdir(path)) if split in shard]
         dataset = ShardedDataset(paths,args.seq_len,args.overlap,args.rank,args.world_size)
-        dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size, num_workers=args.num_workers, worker_init_fn=sharded_init_fn)
+        dataloader = DataLoader(
+                        dataset=dataset, 
+                        batch_size=args.batch_size, 
+                        num_workers=args.num_workers,
+                        worker_init_fn=seed_worker
+                    )
     
     return dataloader
 
--- src/model_utils.py
+++ src/model_utils.py
@@ -12,73 +12,74 @@ def train(model,tokenizer,train_dataloader,eval_dataloader,optimizer,lr_schedule
 
     start_time = time.time()
 
-    for epoch in range(args.epochs):
-    
-        if args.rank == 0:
-            if args.no_wandb:
-                print("-"*40)
-                print(f"Epoch {epoch}")
-            else:
-                wandb.log({"Epoch": epoch})
-
-        for i,(x, y) in enumerate(train_dataloader):
+    with model.join():
+        for epoch in range(args.epochs):
         
-            x, y = x.to(args.device), y.to(args.device)
-
-            if args.world_size > 1:
-                model.require_backward_grad_sync = ((i+1)%args.grad_accumulation_steps == 0) # If true, `loss.backward()` will trigger gradient sync
-
-            logits = model(x)
-
-            loss = F.cross_entropy(logits.view(-1,args.vocab_size), y.view(-1))
-            
-            loss.backward()
-
-            if (i+1)%args.grad_accumulation_steps == 0: # Only take step when gradients have accumulated
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-
-            # -----------VALIDATION-&-LOGGING--------------   
-        
-            if i % (args.eff_batch_per_log*args.grad_accumulation_steps) == 0 and args.rank == 0:
-                current_time = int(time.time()) - int(start_time)
-                eff_batch = i/args.grad_accumulation_steps
+            if args.rank == 0:
                 if args.no_wandb:
                     print("-"*40)
-                    print(f"Effective Batch {eff_batch:.0f}")
-                    print("-"*40)
-                    print(f"Training has been executing for {current_time} seconds.")
-                    print(f"Current training loss is: {loss:.2f}")
+                    print(f"Epoch {epoch}")
                 else:
-                    wandb.log({
-                        "Train Loss": loss.item(),
-                        "Learning Rate": optimizer.param_groups[0]['lr'],
-                        "Time": current_time,
-                        "Effective Batch Number": eff_batch
-                    })
-                
+                    wandb.log({"Epoch": epoch})
 
-            if i % (args.log_per_val*args.eff_batch_per_log*args.grad_accumulation_steps) == 0:  
-                val_loss = evaluate(model, eval_dataloader, args)
+            for i,(x, y) in enumerate(train_dataloader):
             
-                if args.rank == 0:
+                x, y = x.to(args.device), y.to(args.device)
+
+                if args.world_size > 1:
+                    model.require_backward_grad_sync = ((i+1)%args.grad_accumulation_steps == 0) # If true, `loss.backward()` will trigger gradient sync
+
+                logits = model(x)
+
+                loss = F.cross_entropy(logits.view(-1,args.vocab_size), y.view(-1))
                 
+                loss.backward()
+
+                if (i+1)%args.grad_accumulation_steps == 0: # Only take step when gradients have accumulated
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+
+                # -----------VALIDATION-&-LOGGING--------------   
+            
+                if i % (args.eff_batch_per_log*args.grad_accumulation_steps) == 0 and args.rank == 0:
+                    current_time = int(time.time()) - int(start_time)
+                    eff_batch = i/args.grad_accumulation_steps
                     if args.no_wandb:
-                        print(f"Current validation loss is: {val_loss:.2f}")
-                    else:
-                        wandb.log({
-                            "Val Loss": val_loss,
-                        })
                         print("-"*40)
                         print(f"Effective Batch {eff_batch:.0f}")
                         print("-"*40)
+                        print(f"Training has been executing for {current_time} seconds.")
+                        print(f"Current training loss is: {loss:.2f}")
+                    else:
+                        wandb.log({
+                            "Train Loss": loss.item(),
+                            "Learning Rate": optimizer.param_groups[0]['lr'],
+                            "Time": current_time,
+                            "Effective Batch Number": eff_batch
+                        })
                     
-                    print('Sample Output:')
-                    response = generate(model,tokenizer,args.val_prompt,args.temp,args.device)
-                    print(args.val_prompt+response)
+
+                if i % (args.log_per_val*args.eff_batch_per_log*args.grad_accumulation_steps) == 0:  
+                    val_loss = evaluate(model, eval_dataloader, args)
+                
+                    if args.rank == 0:
                     
-            # ----------------------------------------------           
+                        if args.no_wandb:
+                            print(f"Current validation loss is: {val_loss:.2f}")
+                        else:
+                            wandb.log({
+                                "Val Loss": val_loss,
+                            })
+                            print("-"*40)
+                            print(f"Effective Batch {eff_batch:.0f}")
+                            print("-"*40)
+                        
+                        print('Sample Output:')
+                        response = generate(model,tokenizer,args.val_prompt,args.temp,args.device)
+                        print(args.val_prompt+response)
+                        
+                # ----------------------------------------------           
 
     train_time = int(time.time()) - int(start_time)
 
@@ -90,6 +91,7 @@ def evaluate(model, dataloader, args):
     model.eval()
 
     loss_sum = 0
+    nsamples = 0
 
     with torch.no_grad():
         for x, y in dataloader:
@@ -99,9 +101,12 @@ def evaluate(model, dataloader, args):
             logits = model(x)
 
             loss_sum += F.cross_entropy(logits.view(-1,args.vocab_size), y.view(-1))
+            nsamples += 1
 
-        loss = loss_sum/(args.world_size*len(dataloader))
-        dist.all_reduce(loss, op=dist.ReduceOp.SUM)
+        dist.all_reduce(loss_sum)
+        dist.all_reduce(nsamples)
+
+    loss = loss_sum/nsamples
 
     if model_mode:
         model.train()
