68840c574e059fee7eddd2097baaf78f65093ab8
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-07-19 21:39:29 +0100

    Fix:`_save_tokens` not handling all cases correctly
    If the token chunk is larger than shard size problems happen
    Added a while loop and reordered logic to handle this

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -263,43 +263,37 @@ class Tokenizer:
         all_tokens_np = np.empty((shard_size,), dtype=dtype)
         token_count = 0
         if self.rank == 0:
-            progress_bar = None
-        
+            progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+ 
         for tokens in tokens_iter:
 
-            # is there enough space in the current shard for the new tokens?
-            if token_count + len(tokens) < shard_size:
-                # simply append tokens to current shard
-                all_tokens_np[token_count:token_count+len(tokens)] = tokens
-                token_count += len(tokens)
-                # update progress bar
-                if self.rank == 0 and progress_bar is None:
-                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
-
-                if self.rank == 0:
-                    progress_bar.update(len(tokens))
-            else:
+            while token_count + len(tokens) >= shard_size:
                 # write the current shard and start a new one
                 split = "val" if shard_index == 0 else "train"
                 filename = os.path.join(path, f"{self.rank}_{split}_{shard_index:06d}")
                 
                 # split the document into whatever fits in this shard; the remainder goes to next one
                 remainder = shard_size - token_count
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+
                 if self.rank == 0:
                     progress_bar.update(remainder)
-                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                
                 np.save(filename, all_tokens_np)
                 shard_index += 1
 
-                # populate the next shard with the leftovers of the current doc
-                all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
-                token_count = len(tokens)-remainder
+                token_count = 0
+                tokens = tokens[remainder:]
 
                 if self.rank == 0:
                     progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
-                    progress_bar.update(token_count)
 
-
+            # simply append tokens to current shard
+            all_tokens_np[token_count:token_count+len(tokens)] = tokens
+            token_count += len(tokens)
+            
+            if self.rank == 0:
+                progress_bar.update(len(tokens))
 
         if token_count != 0:
             split = "val" if shard_index == 0 else "train"
