556fdbbf51e92189cbc4ff1692aadec566bbdf36
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-08-14 18:14:58 +0100

    Fix: wrong use of importlib

--- utils/model/lr_schedulers/__init__.py
+++ utils/model/lr_schedulers/__init__.py
@@ -2,7 +2,7 @@ import importlib
 
 def get_lr_scheduler(name, optimizer, steps_per_epoch, args):
     
-    lr_scheduler_module = importlib.import_module("lr_schedulers." + name, package=".")
+    lr_scheduler_module = importlib.import_module(f".{name}", package="utils.model.lr_schedulers")
     lr_scheduler = lr_scheduler_module.get_lr_scheduler(optimizer, steps_per_epoch, args)
 
     return lr_scheduler
--- utils/model/optimizers/__init__.py
+++ utils/model/optimizers/__init__.py
@@ -2,7 +2,7 @@ import importlib
 
 def get_optimizer(name, model, args):
     
-    optimizer_module = importlib.import_module("optimizers." + name, package=".")
+    optimizer_module = importlib.import_module(f".{name}", package="utils.model.optimizers")
     optimizer = optimizer_module.get_optimizer(model, args)
 
     return optimizer
