e0cd57eec237283c66ca926df244c5950194ff03
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-08-14 20:35:18 +0100

    Fused AdamW

--- utils/model/optimizers/adamw.py
+++ utils/model/optimizers/adamw.py
@@ -1,5 +1,5 @@
 from torch.optim import AdamW
 
 def get_optimizer(model, args):
-    optimizer = AdamW(params=model.parameters(),weight_decay=args.weight_decay) 
+    optimizer = AdamW(params=model.parameters(),weight_decay=args.weight_decay, fused='cuda' in args.device) 
     return optimizer
