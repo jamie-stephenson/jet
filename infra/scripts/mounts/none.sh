004a58f474a1ae0084c9d9ca8fb4a05020b2d404
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-10-17 15:54:33 +0000

    NFS support

--- infra/scripts/mounts/none.sh
+++ infra/scripts/mounts/none.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-mount_dir="$1"
+drive_addr="$1"
+drive_usr="$2"
+drive_pwd="$3"
+mount_dir="$4"
+
 sudo apt-get update
 echo  Filesystem type "none" given. Assuimg that filesystem is alread mounted at $mount_dir.
