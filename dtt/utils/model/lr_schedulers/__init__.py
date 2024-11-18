bbbc2ee88c9e2054c39ab096dd889e790aa042ca
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-11-18 15:26:21 +0000

    Fix: importing submodules incorrectly

--- dtt/utils/model/lr_schedulers/__init__.py
+++ dtt/utils/model/lr_schedulers/__init__.py
@@ -2,7 +2,7 @@ import importlib
 
 def get_lr_scheduler(name, optimizer, *args, **kwargs):
     
-    lr_scheduler_module = importlib.import_module(f".{name}", package="utils.model.lr_schedulers")
+    lr_scheduler_module = importlib.import_module(f".{name}", package="dtt.utils.model.lr_schedulers")
     lr_scheduler = lr_scheduler_module.get_lr_scheduler(optimizer, *args, **kwargs)
 
     return lr_scheduler
--- dtt/utils/model/optimizers/__init__.py
+++ dtt/utils/model/optimizers/__init__.py
@@ -2,7 +2,7 @@ import importlib
 
 def get_optimizer(name, model, *args, **kwargs):
     
-    optimizer_module = importlib.import_module(f".{name}", package="utils.model.optimizers")
+    optimizer_module = importlib.import_module(f".{name}", package="dtt.utils.model.optimizers")
     optimizer = optimizer_module.get_optimizer(model, *args, **kwargs)
 
     return optimizer
